89da6b81cad3f49271bc1aef6b9ef18d78670e90
BethanyFolino <bethany.folino@gmail.com>
2022-01-30 12:18:45 -0800

    Changed debug in settings.py in order to avoid internal server error with Heroku deployment

--- bugtracker/settings.py
+++ bugtracker/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'bfolino-bugtracker.herokuapp.com']
 
